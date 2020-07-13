9cff952d1a03d7bd9715d0af3cf302f37deec140
hshanx <ms915818993@163.com>
2020-07-13 17:03:30 +0800

    🚑 Hotfix: 不能提交的bug

--- src/components/CommentEditor.vue
+++ src/components/CommentEditor.vue
@@ -298,7 +298,7 @@
                     return;
                 }
                 // comment 需要是 Markdown 格式的
-                this.comment.content = mark(this.comment.content);
+                this.comment.content = marked(this.comment.content);
                 // Submit the comment
                 this.comment.postId = this.targetId;
                 if (this.replyComment) {
